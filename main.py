fea375c709e45e19d3a23846cacd47e2dd63775c
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2024-09-26 09:49:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
-import requests, os, time
+import requests,re,json, os, time
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 scheduler = AsyncIOScheduler(timezone="Asia/kolkata")
 import asyncio
 
 
-headers = {
-  'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
-  'Accept': "application/json, text/plain, */*",
-  'Accept-Encoding': "gzip, deflate, br, zstd",
-  'sec-ch-ua': "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
-  'x-csrf-token': os.environ["xcsrftoken"],
-  'sec-ch-ua-mobile': "?1",
-  'sec-ch-ua-platform': "\"Android\"",
-  'origin': "https://share.streamlit.io",
-  'sec-fetch-site': "same-origin",
-  'sec-fetch-mode': "cors",
-  'sec-fetch-dest': "empty",
-  'referer': "https://share.streamlit.io/",
-  'accept-language': "en-IN,en;q=0.9,en-US;q=0.8,hi;q=0.7",
-  'priority': "u=1, i",
-  'Cookie': os.environ["cookies_header"]
-}
+
 def all_restart_hibernation(x):
+	r=requests.session()
 	url = "https://share.streamlit.io/api/v1/apps"
 	params = {
 	  'workspace_name': "kinshusharma0412"
 	}
-	response = requests.get(url, params=params, headers=headers)
+	r.headers.update({
+  'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36",
+  'Content-Type': "application/json",})
+	r.cookies.update({"streamlit_session":os.environ["cookies_header"]})
+	response = r.get(url, params=params)
+	if "x-csrf-token" in response.headers:
+		r.headers.update({"x-csrf-token":response.headers["x-csrf-token"]})
 	app_=response.json()['apps']
 	app_1=[]
 	for x in app_:
@@ -40,14 +31,14 @@ def all_restart_hibernation(x):
 	
 	for app_id in app_1:
 		url = "https://share.streamlit.io/api/v2/apps/"+str(app_id['appId'])+"/restart"
-		response = requests.post(url, headers=headers)
+		response = r.post(url)
 		print(app_1)
 		print(response.text)
 
 def hibernation(x):
 	url = "https://share.streamlit.io/api/v2/apps/"+str(x)+"/restart"
 	response = requests.post(url, headers=headers)
-	print(response.text)
+	print(response.status_code)
 
 print(scheduler.add_job(all_restart_hibernation, 'interval', minutes=5,args=("x",)))
 
